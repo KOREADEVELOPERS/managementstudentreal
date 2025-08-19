3bf27e6aacf76eebfff865ba042a0e3426f41e8e
KOREADEVELOPERS <sulabhsharma3624@gmail.com>
2025-08-19 21:56:47 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,35 @@
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Div from './COMPONENTS/Div';
+import Homepage_login from './COMPONENTS/Homepage_login';
+import Studentsfeatures from './COMPONENTS/Studentsfeatures';
+ import Addstudents from './COMPONENTS/Addstudents';
+ import Studentservice from './Service/Studentservice';
+ import Viewstudent from './COMPONENTS/Viewstudent'
+ import Searchstudent from './COMPONENTS/Searchstudent';
+ import Deletestudent from './COMPONENTS/Deletestudent';
+ import Registeredpage from './COMPONENTS/Registeredpage';
+  // ✅ Fixed import
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route index element={<Div />} />
+          <Route path="/" element={<Div />} />
+          <Route path="/login" element={<Homepage_login />} />
+          <Route path="/features" element={<Studentsfeatures />} />
+          <Route path="/home" element={<Div />} />
+          <Route path="/addstudents" element={<Addstudents />} />
+          <Route path="/search" element={<Searchstudent />} />
+          <Route path="/sulabh" element={<Viewstudent />} />
+          <Route path="/delete" element={<Deletestudent />} />
+          <Route path="/Register" element={<Registeredpage />} />
+        </Routes>
+      </BrowserRouter>
+    </>
   );
 }
 
