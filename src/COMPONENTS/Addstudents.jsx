a556e842709bfab59f94aab9273db687f368dff5
KOREADEVELOPERS <sulabhsharma3624@gmail.com>
2025-12-11 21:42:03 +0530

    Update Addstudents.jsx

--- src/COMPONENTS/Addstudents.jsx
+++ src/COMPONENTS/Addstudents.jsx
@@ -2,7 +2,7 @@
 import React, { useState, useEffect } from "react";
 import AOS from "aos";
 import "aos/dist/aos.css";
-import { useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";  
 import StudentService from "../Service/Studentservice";
 
 const AddStudents = () => {
